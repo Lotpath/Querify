b6e910aef52eead26def041a086c73d34c07c12a
Jeff Doolittle <jeffdoolittle@gmail.com>
2014-11-05 11:33:03 -0800

    fixing package deployment issues. rolled to next build number.

--- ReleaseNotes.md
+++ ReleaseNotes.md
@@ -1,2 +1,5 @@
-### New in 0.6.0.0 (Release 2014/11/xx)
-* cool stuff
+### New in 0.6.0.1 (Release 2014/11/05)
+* fixing nuget deployment issues
+
+### New in 0.6.0.0 (Release 2014/11/05)
+* working toward package stability
--- src/SolutionInfo.cs
+++ src/SolutionInfo.cs
@@ -7,7 +7,7 @@ using System.Reflection;
 
 [assembly: AssemblyDescription("Query and paging extensions for NHibernate")]
 [assembly: AssemblyProduct("Querify")]
-[assembly: AssemblyVersion("0.6.0.0")]
-[assembly: AssemblyFileVersion("0.6.0.0")]
-[assembly: AssemblyInformationalVersion("0.6.0.0")]
+[assembly: AssemblyVersion("0.6.0.1")]
+[assembly: AssemblyFileVersion("0.6.0.1")]
+[assembly: AssemblyInformationalVersion("0.6.0.1")]
 [assembly: AssemblyCopyright("Copyright (c) Lotpath 2014")]
